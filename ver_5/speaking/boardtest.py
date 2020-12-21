b505440ea3aee07f3ba0ccf05c67a9e83bc1220f
Aryanaut <aryan.mahesh2010@gmail.com>
2020-12-21 23:20:36 +0530

    board

--- ver_5/speaking/boardtest.py
+++ ver_5/speaking/boardtest.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 import dlib
 
+def nothing(x):
+	pass
+
 # definitions
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
@@ -10,7 +13,6 @@ gray = np.zeros((100, 100, 3), np.uint8)
 cv2.createTrackbar('threshold', 'thr', 0, 255, nothing)
 board = np.zeros((297, 420, 3), np.uint8)
 board = cv2.resize(board, None, fx=3, fy=3)
-
 
 GSTREAMER_PIPELINE0 = 'nvarguscamerasrc sensor_id=0 ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=21/1 ! nvvidconv flip-method=0 ! video/x-raw, width=360, height=231, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
 cap = cv2.VideoCapture(GSTREAMER_PIPELINE0, cv2.CAP_GSTREAMER) 
@@ -22,7 +24,7 @@ def main():
     while True:
         ret, frame = cap.read()
         if ret:
-            gray = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             faces = detector(gray)
             if str(faces) == 'rectangles':
                 pass
@@ -33,6 +35,34 @@ def main():
                     cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
 
                     landmarks = predictor(gray, face)
+                    left_point = (landmarks.part(36).x, landmarks.part(36).y)
+                    right_point = (landmarks.part(39).x, landmarks.part(39).y)
+                    top_center = midpoint(landmarks.part(37), landmarks.part(38))
+                    bottom_center = midpoint(landmarks.part(41), landmarks.part(40))
+
+                    #Gaze Ratio
+                    left_eye_region = np.array([(landmarks.part(36).x, landmarks.part(36).y),
+                            (landmarks.part(37).x, landmarks.part(37).y),
+                            (landmarks.part(38).x, landmarks.part(38).y),
+                            (landmarks.part(39).x, landmarks.part(39).y),
+                            (landmarks.part(40).x, landmarks.part(40).y),
+                            (landmarks.part(41).x, landmarks.part(41).y)], np.int32)
+
+                    right_eye_region = np.array([(landmarks.part(42).x, landmarks.part(42).y),
+                            (landmarks.part(43).x, landmarks.part(43).y),
+                            (landmarks.part(44).x, landmarks.part(44).y),
+                            (landmarks.part(45).x, landmarks.part(45).y),
+                            (landmarks.part(46).x, landmarks.part(46).y),
+                            (landmarks.part(47).x, landmarks.part(47).y)], np.int32)
+
+                    min_x = np.min(left_eye_region[:, 0])
+                    max_x = np.max(left_eye_region[:, 0])
+                    min_y = np.min(left_eye_region[:, 1])
+                    max_y = np.max(left_eye_region[:, 1])
+                    left_eye = frame[min_y-1: max_y+3, min_x-3 : max_x+1]
+cv2.getTrackbarPos(
+                    cv2.imshow('eye', left_eye)
+
             cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
