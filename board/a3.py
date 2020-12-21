87ebde743820be1e1aaaf5427ac8d7244594c9b0
Aryan Mahesh <aryan.mahesh2010@gmail.com>
2020-12-21 18:37:29 +0530

    -bleh

---
+++ board/a3.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+board = np.zeros((297, 420, 3), np.uint8)
+board = cv2.resize(board, None, fx=3, fy=3)
+while True:
+    cv2.imshow('board', board)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+cv2.destroyAllWindows()
